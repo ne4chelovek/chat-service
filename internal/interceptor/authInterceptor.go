13ef5bd6378bae3a8d95ac288f204cbde141c856
Ne Chelovek <zimooov@gmail.com>
2025-05-01 18:12:14 +0300

    hot fix

--- internal/interceptor/authInterceptor.go
+++ internal/interceptor/authInterceptor.go
@@ -1,5 +1,20 @@
 package interceptor
 
-type Client struct {
-	Client
+import (
+	"context"
+
+	"google.golang.org/grpc"
+)
+
+type validator interface {
+	Validate() error
+}
+
+func ValidateInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	if val, ok := req.(validator); ok {
+		if err := val.Validate(); err != nil {
+			return nil, err
+		}
+	}
+	return handler(ctx, req)
 }
